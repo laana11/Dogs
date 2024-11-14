278c8989e09818065e73392240fc8ec33c6cc6b4
laana11 <m.pequena@icloud.com>
2024-11-14 17:18:51 +0300

    Создаем главное окно, метку и кнопку

---
+++ Dogs.py
@@ -0,0 +1,17 @@
+from tkinter import *
+import requests
+from PIL import Image, ImageTk
+from io import BytesIO
+
+
+window = Tk()
+window.title("Собачки")
+window.geometry("360x420")
+
+label = Label()
+label.pack(pady=10)
+
+button = Button(text="Загрузить изображение", command=show_image)
+button.pack(pady=10)
+
+window.mainloop()
