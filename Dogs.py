ad14fe85a5e8e4549c7c8384d42d3403b23e22c8
laana11 <m.pequena@icloud.com>
2024-11-14 17:34:58 +0300

    Создана функция загрузки ссылки на изображение

--- Dogs.py
+++ Dogs.py
@@ -13,14 +13,24 @@ def show_image():
             response.raise_for_status()
             img_data = BytesIO(response.content)
             img = Image.open(img_data)
+            img = ImageTk.PhotoImage(img)
             img.thumbnail((300, 300))
             label.config(image=img)
             label.image = img
         except Exception as e:
-            mb.showerror("Ошибка", "Произошла ошибка {e}")
+            mb.showerror("Ошибка", "Произошла ошибка при загрузке изображения {e}")
 
 
-def dog_
+def get_dog_image():
+    try:
+        response = requests.get("https://dog.ceo/api/breeds/image/random")
+        response.raise_for_status()
+        data = response.json()
+        return data("message")
+    except Exception as e:
+        mb.showerror("Ошибка", "Произошла ошибка при запросе к API {e}")
+        return None
+
 
 window = Tk()
 window.title("Собачки")
